5e9d4130d485c920525a0fb14c08f22f0129456e
Manuel de la Torre <manuelmhtr@gmail.com>
2019-11-01 12:14:14 -0600

    v1.0.0

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+const SpottSDK = require('spott-nodejs-sdk')
+
+class GetLocation {
+  constructor(spottApiKey) {
+    assert(spottApiKey, 'A Spott API Key is required');
+    this.spott = new SpottSDK(spottApiKey);
+  }
+
+  byIp(...args) {
+    return this.spott.getPlaceByIp(...args)
+      .catch(handleError);
+  }
+
+  byMyIp(...args) {
+    return this.spott.getPlaceByMyIp(...args)
+      .catch(handleError);
+  }
+};
+
+function handleError(error) {
+  const NOT_FOUND_STATUS = 404;
+  const {status} = (error || {}).response || {};
+
+  console.log('STATUS', status);
+
+  if (status === NOT_FOUND_STATUS) return Promise.resolve(null);
+  return Promise.reject(error);
+}
+
+module.exports = GetLocation;
